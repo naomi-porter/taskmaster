95069b73126da52af8d7339617bead8cb5fbca4b
naomi-porter <naomiporter.dev@gmail.com>
2023-08-15 13:01:31 +1200

    create seasons table

---
+++ server/db/migrations/20230815005829_seasons.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('seasons', (table) => {
+    table.increments('id')
+    table.increments('year')
+    table.text('image')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('seasons')
+};
