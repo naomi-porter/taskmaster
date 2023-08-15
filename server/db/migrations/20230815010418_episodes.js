8f474503048c7cc49897ec2ec3a0030743bd5b02
naomi-porter <naomiporter.dev@gmail.com>
2023-08-15 13:10:11 +1200

    create episodes table

---
+++ server/db/migrations/20230815010418_episodes.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('episodes', (table) => {
+    table.increments('id')
+    table.increments('season')
+    table.string('title')
+    table.text('image')
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('episodes')
+};
