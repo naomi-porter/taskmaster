91973526bd80b018a2b9ea788b56b573081aeee8
naomi-porter <naomiporter.dev@gmail.com>
2023-08-18 14:35:39 +1200

    task: FE routes completed

---
+++ client/routes.tsx
@@ -0,0 +1,26 @@
+// page directory
+// list all of the pages
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Outlet
+} from 'react-router-dom'
+
+import App from './components/App'
+import Season from './components/Season'
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+
+      <Route path='/'>
+        <Route path='/' element={<App/>}/>
+        <Route path='/seasons/:id' element={<Season/>}/>
+      </Route>
+
+  
+  ),
+)
+
+export default routes
