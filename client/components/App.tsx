43c665b91bc9b5ce96063ab8666f0aaefc312e89
naomi-porter <naomiporter.dev@gmail.com>
2023-08-18 14:37:26 +1200

    start components

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,25 @@
+// import { fetchSeasons } from "../apis/apis"
+// import { useState } from "react"
+
+
+
 function App() {
   return (
     <>
       <header className="header">
-        <h1>My Collection</h1>
+        <h1 className="title">Taskmaster</h1>
       </header>
-      <section className="main">{/* add your code here */}</section>
+
+      <section className="main">
+        <h2 className="subtitle">Seasons</h2>
+        <div className="columns">
+          <div className="column">First column</div>
+          <div className="column">Second column</div>
+          <div className="column">Third column</div>
+          <div className="column">Fourth column</div>
+          <div className="column">5th column</div>
+        </div>
+      </section>
     </>
   )
 }
---
+++ client/components/Season.tsx
@@ -0,0 +1,29 @@
+function Season() {
+  return (
+    <>
+      <header className="header">
+        <h1 className="title">Season number</h1>
+      </header>
+      <section className="main">
+        <h2 className="subtitle">Contestants</h2>
+        <div className="columns">
+          <div className="column">First column</div>
+          <div className="column">Second column</div>
+          <div className="column">Third column</div>
+          <div className="column">Fourth column</div>
+          <div className="column">5th column</div>
+        </div>
+        <h2 className="subtitle">Episodes</h2>
+        <div className="columns">
+          <div className="column">First column</div>
+          <div className="column">Second column</div>
+          <div className="column">Third column</div>
+          <div className="column">Fourth column</div>
+          <div className="column">5th column</div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Season
